2fca7f644978a1a4a8660c2c3f212ec501277f1a
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2019-05-15 13:28:56 -0700

    added project pages

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ $(function () {
             const nextPos = (currentScreen + delta) * $(window).height() - 70;
 
             //animate
-            $('html, body').animate({ scrollTop: nextPos }, 750, 'easeInOutExpo', () => {
+            $('html, body').animate({ scrollTop: nextPos }, 2000, 'easeInOutExpo', () => {
                 scrollComplete = true;
             });
         }
